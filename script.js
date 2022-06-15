8ec9695b72016c980ae80abcb821def7f8c2498c
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2022-06-15 12:30:32 +0800

    Setting up the key and value
    Setting the DOM with javascript into HTML, still figuring out how to access the currency values

--- script.js
+++ script.js
@@ -5,7 +5,46 @@ searchBtn.addEventListener("click", () => {
     let countryName = "Brunei";
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalURL)
-    fetch(finalURL).then((response) => response.json()).then((data) => {
+    fetch(finalURL).then((response) => response.json())
+    .then((data) => {
         console.log(data);
+        console.log(data[0]);
+        console.log(data[0].capital[0]);
+        console.log(data[0].flags.svg);
+        console.log(data[0].name.common);
+        console.log(data[0].continents[0]);
+        console.log(Object.keys(data[0].currencies)[0]);
+        console.log(data[0].timezones[0]);
+        console.log(Object.values(data[0].languages).toString().split(",").join(","));
+        result.innerHTML = `
+        <img src="${data[0].flags.svg}"
+        class="flag-img">
+        <h1>${data[0].name.common}</h1>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h3>Capital: </h3>
+                <span>${data[0].capital[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h3>Continent: </h3>
+                <span>${data[0].continents[0]}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h3>Population: </h3>
+                <span>${data[0].population}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h3>Currency: </h3>
+                <span>${Object.keys(data[0].currencies)[0] }</span>
+            </div>
+        </div>        
+        `;
+        // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
     });
 });
