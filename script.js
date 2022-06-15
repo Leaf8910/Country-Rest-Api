7f256672c36af6bd84434e5d5b827caa617ecdc9
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2022-06-15 14:42:42 +0800

    Added coatofarms svg, languages and fixed the h tags and css
    Added heavier css so it stands out more, removed the console logs and lets the user input the wanted country in the input text field

--- script.js
+++ script.js
@@ -2,49 +2,62 @@ let searchBtn = document.getElementById("search-btn");
 let countryInput = document.getElementById("country-inp");
 
 searchBtn.addEventListener("click", () => {
-    let countryName = "Brunei";
+    let countryName = countryInput.value;
     let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
     console.log(finalURL)
     fetch(finalURL).then((response) => response.json())
     .then((data) => {
-        console.log(data);
-        console.log(data[0]);
-        console.log(data[0].capital[0]);
-        console.log(data[0].flags.svg);
-        console.log(data[0].name.common);
-        console.log(data[0].continents[0]);
-        console.log(Object.keys(data[0].currencies)[0]);
-        console.log(data[0].timezones[0]);
-        console.log(Object.values(data[0].languages).toString().split(",").join(","));
         result.innerHTML = `
         <img src="${data[0].flags.svg}"
         class="flag-img">
-        <h1>${data[0].name.common}</h1>
+        <h2>${data[0].name.common}</h2>
         <div class="wrapper">
             <div class="data-wrapper">
-                <h3>Capital: </h3>
+                <h4>Capital: </h4>
                 <span>${data[0].capital[0]}</span>
             </div>
         </div>
         <div class="wrapper">
             <div class="data-wrapper">
-                <h3>Continent: </h3>
+                <h4>Continent: </h4>
                 <span>${data[0].continents[0]}</span>
             </div>
         </div>
         <div class="wrapper">
             <div class="data-wrapper">
-                <h3>Population: </h3>
+                <h4>Population: </h4>
                 <span>${data[0].population}</span>
             </div>
         </div>
         <div class="wrapper">
             <div class="data-wrapper">
-                <h3>Currency: </h3>
+                <h4>Currency: </h4>
                 <span>${Object.keys(data[0].currencies)[0] }</span>
             </div>
-        </div>        
+        </div>  
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Common Languages: </h4>
+                <span>${Object.values(data[0].languages).toString().split(",").join(",")}</span>
+            </div>
+        </div>
+        <div class="wrapper">
+            <div class="data-wrapper">
+                <h4>Coat of Arms: </h4>
+                <span><img src="${data[0].coatOfArms.svg}"
+                class="coat-img"></span>
+            </div>
+        </div>
+            
         `;
         // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
+    })
+    .catch(() => {
+        if(countryName.length == 0){
+            result.innerHTML = `<h3>This input field cannot be empty</h3>`;
+        }
+        else {
+            result.innerHTML = `<h3>No country found</h3>`;
+        }
     });
 });
