8493994c7dc42819e031abc80afa61cc4318d421
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2022-06-16 19:47:42 +0800

    Replaced search with icon and added timezone
    Added a coat of arms svg, figuring out how to make it work with visibility toggle

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInput = document.getElementById("country-inp");
+let coatSvg = document.getElementById("coatSvg");
+let coatLoad = document.getElementById("coatLoad")
+
 
 searchBtn.addEventListener("click", () => {
     let countryName = countryInput.value;
@@ -43,8 +46,15 @@ searchBtn.addEventListener("click", () => {
         </div>
         <div class="wrapper">
             <div class="data-wrapper">
+                <h4 display="none">Timezone: </h4>
+                <span>${Object.values(data[0].timezones)[0] }</span>
+            </div>
+        </div>  
+        <div class="wrapper">
+            <div class="data-wrapper">
                 <h4>Coat of Arms: </h4>
-                <span><img src="${data[0].coatOfArms.svg}"
+                <svg id="coatLoad" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.354 10.146a.5.5 0 010 .708l-3 3a.5.5 0 01-.708 0l-3-3a.5.5 0 01.708-.708L8 12.793l2.646-2.647a.5.5 0 01.708 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M8 6a.5.5 0 01.5.5V13a.5.5 0 01-1 0V6.5A.5.5 0 018 6zM2 3.5a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z" clip-rule="evenodd"></path></svg>
+                <span><img id="coatSvg" display="block" src="${data[0].coatOfArms.svg}"
                 class="coat-img"></span>
             </div>
         </div>
@@ -61,3 +71,8 @@ searchBtn.addEventListener("click", () => {
         }
     });
 });
+
+// coatLoad.addEventListener("click", () => {
+//     coatSvg.style.display = "contents";
+//     coatLoad.style.display = "none";
+// })
