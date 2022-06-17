d2a9a5ba299d3b7dbf6950f341c43bfb8e9e5a3e
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2022-06-17 15:16:28 +0800

    Added background gradient
    Background gradient becomes reactive and tried adding an svg button icon where it toggles the svg coat of arms

--- script.js
+++ script.js
@@ -54,11 +54,10 @@ searchBtn.addEventListener("click", () => {
             <div class="data-wrapper">
                 <h4>Coat of Arms: </h4>
                 <svg id="coatLoad" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M11.354 10.146a.5.5 0 010 .708l-3 3a.5.5 0 01-.708 0l-3-3a.5.5 0 01.708-.708L8 12.793l2.646-2.647a.5.5 0 01.708 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M8 6a.5.5 0 01.5.5V13a.5.5 0 01-1 0V6.5A.5.5 0 018 6zM2 3.5a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z" clip-rule="evenodd"></path></svg>
-                <span><img id="coatSvg" display="block" src="${data[0].coatOfArms.svg}"
+                <span><img id="coatSvg" display="none" src="${data[0].coatOfArms.svg}"
                 class="coat-img"></span>
             </div>
         </div>
-            
         `;
         // console.log(data[0].currencies[Object.keys(data[0].currencies)].name);
     })
@@ -72,7 +71,15 @@ searchBtn.addEventListener("click", () => {
     });
 });
 
-// coatLoad.addEventListener("click", () => {
-//     coatSvg.style.display = "contents";
-//     coatLoad.style.display = "none";
-// })
+
+
+if (coatLoad) {
+    coatLoad.addEventListener("click", () => {
+        if (coatSvg.style.display === "none") {
+        coatSvg.style.display = "block";
+        console.log("clicked")
+        } else {
+            coatSvg.style.display = "none";
+        }
+    })
+} 
